1bebbd128a8953eb58b9c234e15fbc77be884b2b
Chris King <chrisking@yapstudios.com>
2017-06-08 15:49:23 -0700

    Initial Commit

---
+++ SuperShadowrun/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SuperShadowrun
+//
+//  Created by Chris King on 6/8/17.
+//  Copyright © 2017 Cirion. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
